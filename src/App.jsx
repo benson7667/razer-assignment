a7be93cb9eddf9eb38be935582e357de68bb822b
Benson Toh <benson@eatigo.com>
2020-04-24 13:35:18 +0800

    create own sidebar css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { SideBar, Content } from "./shared/layouts";
 
 const App = () => {
   return (
-    <div className="main-container">
-      <div className="thx-wrapper flex">
-        <SideBar />
-        <Content />
-      </div>
+    // <div className="main-container">
+    <div className="flex">
+      <SideBar />
+      <Content />
     </div>
+    // </div>
   );
 };
 
--- src/shared/layouts/sidebar/SideBar.jsx
+++ src/shared/layouts/sidebar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { func } from "prop-types";
+import { FiHome } from "react-icons/fi";
 
 import ActionToolbar from "./components/action_toolbar";
 import MenuList from "./components/menu_list";
@@ -13,13 +14,28 @@ class SideBar extends Component {
 
   render() {
     return (
-      <div className="thx-drawer flex">
-        <div className="main-title">Profile List</div>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-title">Profile List</div>
 
-        <div className="drawer-select">
+        <ul className="sidebar-menu-list">
+          <li className="sidebar-menu-list-item">
+            <FiHome className="mr12" color="#44d62c" />
+            <span>Default</span>
+          </li>
+          <li className="sidebar-menu-list-item">
+            <FiHome className="mr12" color="#44d62c" />
+            <span>Game</span>
+          </li>
+          <li className="sidebar-menu-list-item">
+            <FiHome className="mr12" color="#44d62c" />
+            <span>Music</span>
+          </li>
+        </ul>
+
+        {/* <div className="drawer-select">
           <MenuList />
           <ActionToolbar />
-        </div>
+        </div> */}
       </div>
     );
   }
